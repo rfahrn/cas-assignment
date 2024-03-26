891aaebb167808e4dc9cd31e37f7f5113d5726dc
rfahrn <rebecka.fahrni16@gmail.com>
2024-03-26 18:46:00 +0100

    update

--- assignments/toolcalibration/calibration.py
+++ assignments/toolcalibration/calibration.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import svd
 
 
 def pivot_calibration(transforms):
@@ -17,8 +18,23 @@ def pivot_calibration(transforms):
 
     ## TODO: Implement pivot calibration as discussed in the lecture
 
-    T = np.eye(4)
-
+    # 1. extract rotation matrix R and translation vector p from each transformation matrix.
+    # 2. construct matrix A and vector b
+    # 3. solve for x in Ax = b
+    
+    T = np.eye(4) # identity matrix
+    n = len(transforms) # number transfforms
+    A = np.zeros((3*n, 6)) #  A
+    b = np.zeros((3*n, 1)) # b
+    for i, Fi in enumerate(transforms):
+        Ri = Fi[:3, :3] # rotation matrix
+        pi = Fi[:3, 3].reshape(3, 1) # translation vector 
+        A[i*3:(i+1)*3, :3] = Ri 
+        A[i*3:(i+1)*3, 3:] = -np.eye(3)
+        b[i*3:(i+1)*3, :] = -pi
+    U, s, V = svd(A, full_matrices=False)
+    x = np.dot(V.T, np.dot(np.diag(1/s), np.dot(U.T, b)))
+    T[:3, 3] = x[:3, 0] 
     return T
 
 
@@ -36,7 +52,8 @@ def calibration_device_calibration(camera_T_reference, camera_T_tool, reference_
     """
 
     ## TODO: Implement a calibration method which uses a calibration device
-
     T = np.eye(4)
-    
+    inv_camera_T_tool = np.linalg.inv(camera_T_tool) 
+    # calibration T from tool to tip using given formula
+    T = inv_camera_T_tool @ camera_T_reference @ reference_T_tip
     return T
